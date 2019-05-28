e61c3a573622fa7354120644bdf1bb18965f456b
wufan <wufannet@qq.com>
2019-05-28 15:39:06 +0800

    添加Person.kt,PersonJ.java

--- lib/src/main/java/com/zaozaofan/lib/Person.kt
+++ lib/src/main/java/com/zaozaofan/lib/Person.kt
@@ -1,4 +1,14 @@
 package com.zaozaofan.lib
 
-data class Person(val name: String,val age: Int? = null)
+data class Person(val name: String, val age: Int? = null) //少了好多航代码
+
+fun main(args: Array<String>) {
+    val person = listOf(Person("a"), Person("b", 29))   //list数据接口
+
+    val oldest = person.maxBy { it.age ?: 0 }  //不需要自己去实现比较器排序和过滤器.工具类方法.
+
+    println("the oldest is: $oldest")
+}
+
+
 
---
+++ lib/src/main/java/com/zaozaofan/lib/PersonJ.java
@@ -0,0 +1,7 @@
+package com.zaozaofan.lib;
+
+public class PersonJ {
+    public static void main(String[] args) {
+
+    }
+}
