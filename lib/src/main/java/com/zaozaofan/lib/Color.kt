21a54aaadecf0880c3a9aa45db080c737860ae00
wufan <wufannet@qq.com>
2019-05-29 14:50:38 +0800

    color fix 没有定义返回所以没有答应, 方法体函数改为表达式函数

--- lib/src/main/java/com/zaozaofan/lib/Color.kt
+++ lib/src/main/java/com/zaozaofan/lib/Color.kt
@@ -3,12 +3,12 @@ package com.zaozaofan.lib
 import java.lang.Exception
 
 //enum class, 自带当前类型常量的类.
-enum class Color (val r:Int, val g:Int, var b:Int){ //属性
-    RED(255,0,0),
-    GREEN(0,255,0),
-    BLUE(0,0,255);  //分号分开 枚举常量列表和方法定义
+enum class Color(val r: Int, val g: Int, var b: Int) { //属性
+    RED(255, 0, 0),
+    GREEN(0, 255, 0),
+    BLUE(0, 0, 255);  //分号分开 枚举常量列表和方法定义
 
-    fun rgb()  = (r*256 +g)*256+b //方法
+    fun rgb() = (r * 256 + g) * 256 + b //方法
 }
 
 fun main() {
@@ -16,11 +16,9 @@ fun main() {
     println(getMnemonic(Color.RED)) //kotlin.Unit
 }
 
-fun getMnemonic(color: Color){
-    when(color){
+fun getMnemonic(color: Color) =
+    when (color) {
         Color.RED -> "rrr"
         Color.GREEN -> "gggg"
         Color.BLUE -> "bbbb"
-        else -> throw Exception("dirty color")
     }
-}
