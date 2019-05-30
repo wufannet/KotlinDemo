3ef6176fd888113d2db19205c04f94d11f6dc2d5
wufan <wufannet@qq.com>
2019-05-30 14:21:07 +0800

    5.30 Hello,Lambda!

--- app/src/main/java/com/zaozaofan/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/zaozaofan/kotlindemo/MainActivity.kt
@@ -2,11 +2,17 @@ package com.zaozaofan.kotlindemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mTv1: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mTv1 = findViewById(R.id.mTv1)
+        mTv1.setOnClickListener { println("lambda") }
     }
 }
